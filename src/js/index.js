61a1101c0e1ab3be9399eaceeb804f5e42070acc
Yuliia Koriavets <112959119+YuliiaKoriavets@users.noreply.github.com>
2023-01-04 21:32:56 +0200

    update homework

---
+++ src/js/index.js
@@ -0,0 +1,70 @@
+'use strict';
+import '../css/styles.css';
+import { fetchPhotos } from './pixabay-api';
+import createGalleryCards from '../templates/gallery-card.hbs';
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchFormEl = document.querySelector('.search-form');
+// const inputEl = document.querySelector('input');
+// const submitBtnEl = document.querySelector('button');
+const galleryContainerEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
+
+let simplelightboxGallery = new SimpleLightbox('.gallery a');
+
+let searchQuery = '';
+let page = 1;
+
+function createPhotoGallery() {
+  fetchPhotos(searchQuery, page).then(({ hits, totalHits }) => {
+    let totalPages = totalHits / 40;
+    if (totalHits === 0) {
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    galleryContainerEl.insertAdjacentHTML(
+      'beforeend',
+      createGalleryCards(hits)
+    );
+    simplelightboxGallery.refresh();
+    loadMoreBtnEl.classList.remove('is-hidden');
+
+    if (page === 2) {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+    if (page === totalPages) {
+      loadMoreBtnEl.classList.add('is-hidden');
+      Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+  });
+}
+
+function updateGallery() {
+  galleryContainerEl.innerHTML = '';
+  page = 1;
+  loadMoreBtnEl.classList.add('is-hidden');
+}
+
+const handleSearchGalleryCards = event => {
+  event.preventDefault();
+  updateGallery();
+  searchQuery = event.target.searchQuery.value;
+
+  if (searchQuery === '') {
+    return Notify.warning(
+      'Oops, the input field is empty. Please enter search query again.'
+    );
+  }
+
+  createPhotoGallery();
+};
+searchFormEl.addEventListener('submit', handleSearchGalleryCards);
+
+const handleClickMoreCards = event => {
+  page += 1;
+  createPhotoGallery();
+};
+loadMoreBtnEl.addEventListener('click', handleClickMoreCards);
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,12 @@
+'use strict';
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '32583880-eecf7687874c4983951318f6f';
+
+export async function fetchPhotos(searchQuery, page) {
+  const { data } = await axios.get(
+    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  );
+  return data;
+}
